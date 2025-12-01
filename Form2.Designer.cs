bf84f3e967ee9286ba1ed7584b1dc97a2773efe9
Elif <198382528+Elif999@users.noreply.github.com>
2025-12-01 23:13:00 +0300

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace WinFormsApp48
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox6 = new GroupBox();
+            label18 = new Label();
+            button11 = new Button();
+            listBox11 = new ListBox();
+            button12 = new Button();
+            listBox12 = new ListBox();
+            label11 = new Label();
+            label12 = new Label();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            listBox2 = new ListBox();
+            label2 = new Label();
+            label3 = new Label();
+            button3 = new Button();
+            groupBox2 = new GroupBox();
+            label4 = new Label();
+            button4 = new Button();
+            listBox3 = new ListBox();
+            button5 = new Button();
+            listBox4 = new ListBox();
+            label5 = new Label();
+            label6 = new Label();
+            groupBox6.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox6
+            // 
+            groupBox6.BackColor = Color.FromArgb(255, 192, 192);
+            groupBox6.Controls.Add(label18);
+            groupBox6.Controls.Add(button11);
+            groupBox6.Controls.Add(listBox11);
+            groupBox6.Controls.Add(button12);
+            groupBox6.Controls.Add(listBox12);
+            groupBox6.Controls.Add(label11);
+            groupBox6.Controls.Add(label12);
+            groupBox6.Location = new Point(295, 9);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(348, 249);
+            groupBox6.TabIndex = 16;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "dizinin indexlerini ve diziye rastgele atılan elemanları kopyala";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(60, 231);
+            label18.Name = "label18";
+            label18.Size = new Size(145, 15);
+            label18.TabIndex = 15;
+            label18.Text = "/*tek boyutlu dizi örneği*/";
+            // 
+            // button11
+            // 
+            button11.Location = new Point(150, 185);
+            button11.Name = "button11";
+            button11.Size = new Size(80, 23);
+            button11.TabIndex = 10;
+            button11.Text = "kopyala";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // listBox11
+            // 
+            listBox11.FormattingEnabled = true;
+            listBox11.ItemHeight = 15;
+            listBox11.Location = new Point(14, 40);
+            listBox11.Name = "listBox11";
+            listBox11.Size = new Size(104, 139);
+            listBox11.TabIndex = 0;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(14, 183);
+            button12.Name = "button12";
+            button12.Size = new Size(80, 23);
+            button12.TabIndex = 1;
+            button12.Text = "ekle";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // listBox12
+            // 
+            listBox12.FormattingEnabled = true;
+            listBox12.ItemHeight = 15;
+            listBox12.Location = new Point(150, 40);
+            listBox12.Name = "listBox12";
+            listBox12.Size = new Size(104, 139);
+            listBox12.TabIndex = 2;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(14, 22);
+            label11.Name = "label11";
+            label11.Size = new Size(69, 15);
+            label11.TabIndex = 3;
+            label11.Text = "Kopyalanan";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(150, 21);
+            label12.Name = "label12";
+            label12.Size = new Size(40, 15);
+            label12.TabIndex = 4;
+            label12.Text = "Kopya";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(255, 192, 192);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(listBox1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(listBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(277, 249);
+            groupBox1.TabIndex = 17;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "diziye atılan rastgele elemanları kopyala";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(60, 231);
+            label1.Name = "label1";
+            label1.Size = new Size(145, 15);
+            label1.TabIndex = 15;
+            label1.Text = "/*tek boyutlu dizi örneği*/";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(150, 185);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 23);
+            button1.TabIndex = 10;
+            button1.Text = "kopyala";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(14, 40);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(104, 139);
+            listBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(14, 183);
+            button2.Name = "button2";
+            button2.Size = new Size(80, 23);
+            button2.TabIndex = 1;
+            button2.Text = "ekle";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(150, 40);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(104, 139);
+            listBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Kopyalanan";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(150, 21);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Kopya";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(530, 499);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 23);
+            button3.TabIndex = 16;
+            button3.Text = "önceki sayfaya git";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(255, 192, 192);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Controls.Add(listBox3);
+            groupBox2.Controls.Add(button5);
+            groupBox2.Controls.Add(listBox4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Location = new Point(12, 267);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(501, 255);
+            groupBox2.TabIndex = 17;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "diziye atılan rastgele elemanları koyalar";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(437, 15);
+            label4.TabIndex = 15;
+            label4.Text = "/*tek boyutlu dizi örneği*/ sadece bu group box bilişim teklonojileri youtubundan";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(14, 185);
+            button4.Name = "button4";
+            button4.Size = new Size(80, 23);
+            button4.TabIndex = 10;
+            button4.Text = "ekle";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(14, 40);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(104, 139);
+            listBox3.TabIndex = 0;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(150, 185);
+            button5.Name = "button5";
+            button5.Size = new Size(80, 23);
+            button5.TabIndex = 1;
+            button5.Text = "kopya";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 15;
+            listBox4.Location = new Point(150, 40);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(104, 139);
+            listBox4.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(14, 22);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 15);
+            label5.TabIndex = 3;
+            label5.Text = "Kopyalanan";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(150, 21);
+            label6.Name = "label6";
+            label6.Size = new Size(40, 15);
+            label6.TabIndex = 4;
+            label6.Text = "Kopya";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(963, 549);
+            Controls.Add(groupBox2);
+            Controls.Add(button3);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox6);
+            Name = "Form2";
+            Text = "Form2";
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox6;
+        private Label label18;
+        private Button button11;
+        private ListBox listBox11;
+        private Button button12;
+        private ListBox listBox12;
+        private Label label11;
+        private Label label12;
+        private GroupBox groupBox1;
+        private Label label1;
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+        private ListBox listBox2;
+        private Label label2;
+        private Label label3;
+        private Button button3;
+        private GroupBox groupBox2;
+        private Label label4;
+        private Button button4;
+        private ListBox listBox3;
+        private Button button5;
+        private ListBox listBox4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp48
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int[] dizi = new int[30];//groupbox1
+
+        int[] dizi1 = new int[35];//groubbox6
+
+
+        int[] dizikaynak = new int[25];//groubbox2
+        int[] dizikopya = new int[25];//groubbox2
+        
+        //*****************************************************groubbox1***************************************************************************************************//
+        private void button1_Click(object sender, EventArgs e)//kopyala
+        {
+            foreach (int kopyala in listBox1.Items)
+            {
+                listBox2.Items.Add(kopyala);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//ekle
+        {
+            Random rastgele = new Random();
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = rastgele.Next(1, 100);
+                listBox1.Items.Add(dizi[i]);
+            }
+
+        }
+
+
+
+
+
+
+
+        //***************************************************************groubbox6******************************************************************************************************//
+
+        private void button12_Click(object sender, EventArgs e)//ekle 
+        {
+            Random rastgele = new Random();
+            for (int i = 0; i < dizi1.Length; i++)
+            {
+                dizi1[i] = rastgele.Next(1, 100);
+                listBox11.Items.Add(i + "=>" + dizi1[i]);
+            }
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)//kopyala
+        {
+            foreach (object kopyala in listBox11.Items)
+            {
+                listBox12.Items.Add(kopyala);
+            }
+        }
+
+       
+
+
+
+
+
+
+//*********************************groubbox2*************************************************************************************//
+
+        private void button5_Click(object sender, EventArgs e)//kopya
+        {
+            for (int i = 0; i < dizikopya.Length; i++)//3.yol
+            {
+                dizikopya[i] = dizikaynak[i];
+            }
+            for (int i = 0; i < dizikopya.Length; i++)
+            {
+                listBox4.Items.Add(dizikopya[i]);
+            }
+            
+            /* foreach(int kopyala in dizikaynak)//1.yol
+                {
+                    listBox4.Items.Add(kopyala);
+                }
+                */
+
+          /*  for (int i = 0; i < dizikopya.Length; i++)//2.yol
+            {
+                dizikopya[i] = dizikaynak[i];
+                listBox4.Items.Add(dizikopya[i]);
+            }
+          */
+        }
+
+        private void button4_Click(object sender, EventArgs e)//ekle
+        {
+            /* Random rastgele = new Random();//1.yol
+             for(int i=0;i<dizikaynak.Length;i++)
+             {
+                 dizikaynak[i] = rastgele.Next(1, 100);
+                 listBox3.Items.Add(dizikaynak[i]);
+             }
+            */
+            Random rastgele = new Random();//2.yol
+            for ( int i = 0; i < dizikaynak.Length; i++)
+            {
+                dizikaynak[i] = rastgele.Next(1, 100);
+            }
+            for(int i=0;i<dizikaynak.Length;i++)
+            {
+                listBox3.Items.Add(dizikaynak[i]);
+            }
+            
+
+
+        }
+        private void button3_Click(object sender, EventArgs e)//önceki sayfa
+        {
+            Form1 fr1 = new Form1();
+            fr1.Show();
+            this.Hide();
+        }
+    }
+}
